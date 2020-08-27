e536a4cad9781e0c1340a52f80e910d5c70ae38a
Hugo <hugo-pires@outlook.com>
2020-08-26 23:29:30 -0300

    database config

--- src/main/kotlin/io/github/hugoltsp/stonks/infra/config/DatabaseConfig.kt
+++ src/main/kotlin/io/github/hugoltsp/stonks/infra/config/DatabaseConfig.kt
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource
 import io.github.hugoltsp.stonks.infra.domain.Settings
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Database.Companion.connect
+import java.net.URI
 
 object DatabaseConfig {
 
@@ -15,20 +16,31 @@ object DatabaseConfig {
         if (!::connection.isInitialized) {
 
             connection = connect(
-                HikariDataSource(
-                    HikariConfig().apply {
-                        driverClassName = "org.postgresql.Driver"
-                        jdbcUrl = Settings.jdbcUrl
-                        username = Settings.databaseUser
-                        password = Settings.databasePassword
-                        connectionTestQuery = "SELECT 1"
-                    }
-                )
+                HikariDataSource(hikariConfig())
             )
 
         }
 
         return connection
+    }
+
+    private fun hikariConfig() = System.getenv("DATABASE_URL")?.let {
+        val dbUri = URI(it)
+        val usernameDb = dbUri.userInfo.split(":")[0]
+        val passwordDb = dbUri.userInfo.split(":")[1]
+        HikariConfig().apply {
+            driverClassName = "org.postgresql.Driver"
+            jdbcUrl = "jdbc:postgresql://${dbUri.host}:${dbUri.port}${dbUri.path}"
+            username = usernameDb
+            password = passwordDb
+            connectionTestQuery = "SELECT 1"
+        }
+    } ?: HikariConfig().apply {
+        driverClassName = "org.postgresql.Driver"
+        jdbcUrl = Settings.jdbcUrl
+        username = Settings.databaseUser
+        password = Settings.databasePassword
+        connectionTestQuery = "SELECT 1"
     }
 
 }
