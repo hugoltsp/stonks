26552fea12aa807db7d250415622a7103c1ff3b8
Hugo <hugo-pires@outlook.com>
2020-08-27 14:06:57 -0300

    google br

--- src/main/kotlin/io/github/hugoltsp/stonks/data/resource/StockResource.kt
+++ src/main/kotlin/io/github/hugoltsp/stonks/data/resource/StockResource.kt
@@ -9,7 +9,7 @@ import java.math.RoundingMode
 
 object StockResource {
 
-    private const val GOOGLE_URL = "https://www.google.com/search"
+    private const val GOOGLE_URL = "https://www.google.com.br/search"
     private const val USER_AGENT =
         "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
     private val logger = getLogger()
